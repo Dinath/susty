64a8e761b4f4a88a32645467080116f0032ee496
Dinath <dinath.contact@gmail.com>
2020-05-08 18:19:43 +0200

    :mag: Preconnect Google Analytics.

--- header.php
+++ header.php
@@ -13,8 +13,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <link rel="preconnect" href="https://www.google-analytics.com">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
