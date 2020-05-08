e28e5f3413d48573328e2a76b5385cb8091c4f67
Dinath <dinath.contact@gmail.com>
2020-05-08 13:12:53 +0200

    :art: Rewriting header and navbar.

--- header.php
+++ header.php
@@ -20,50 +20,38 @@
 <body <?php body_class(); ?>>
 
 <div id="page">
+	
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'susty' ); ?></a>
 
 	<header id="masthead">
-		<div class="logo">
-			<?php
-			if ( has_custom_logo() ) :
-				the_custom_logo();
-			else :
-				?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img alt="Susty WP logo" src="<?php echo esc_url( get_template_directory_uri() . '/images/eco-chat.svg' ); ?>"><span class="screen-reader-text"><?php esc_html_e( 'Home', 'susty' ); ?></span></a>
-				<?php
-			endif;
-			?>
-		</div>
-
 		<?php
-		if ( is_front_page() && is_home() && ! get_query_var( 'menu' ) ) :
+		if ( is_front_page() && is_home() ) :
 			?>
-			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); ?></a></h1>
 			<?php
 		else :
 			?>
-			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); ?></a></p>
 			<?php
 		endif;
+		?>
 
+		<p><?php echo bloginfo( 'description' ); ?></p>
+
+		<?php
 		if ( has_nav_menu( 'menu-1' ) ) :
-
-			if ( get_query_var( 'menu' ) ) :
-				?>
-				<a id="susty-back-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&#x2716;<span class="screen-reader-text"><?php esc_html_e( 'Close menu', 'susty' ); ?></span></a>
-				<script>
-					var susty_home_url = '<?php echo esc_url( home_url( '/' ) ); ?>';
-					if ( 0 === document.referrer.indexOf( susty_home_url ) ) {
-						document.getElementById( 'susty-back-link' ).href = document.referrer;
-					}
-				</script>
-				<?php
-			else :
-				?>
-				<a href="<?php echo esc_url( ( get_option( 'permalink_structure' ) ? home_url( '/menu/' ) : home_url( '/?menu' ) ) ); ?>"><?php esc_html_e( 'Menu', 'susty' ); ?></a>
-				<?php
-			endif;
-		
+			?>
+			<nav>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+			</nav>
+			<?php
 		endif;
 		?>
 	</header>
