57848a8c00c3a6b63e33caad478ef5984864dbe6
Dinath <dinath.contact@gmail.com>
2020-06-07 14:45:21 +0200

    :sparkles: Add front page design :lipstick:

--- front-page.php
+++ front-page.php
@@ -10,21 +10,23 @@
 get_header();
 ?>
 	<main id="primary" class="site-main">
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
 			get_template_part( 'template-parts/content', 'home' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+		endwhile;
 		?>
-
+		<?php
+		if ( is_front_page() && ! is_home() ) :
+			?>
+			<div class="aligncenter">
+				<a href="<?php echo get_post_type_archive_link( 'post' ); ?>" class="button"><?php esc_html_e( 'All posts.', 'susty' ); ?></a>
+			</div>
+			<?php
+		else :
+            get_template_part('template-parts/pagination');
+		endif;
+		?>
 	</main><!-- #main -->
 <?php
 get_sidebar();
