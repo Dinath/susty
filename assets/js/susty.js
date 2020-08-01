beb3a9463a40424777a6fcd1b36dc95732b871c6
= <=>
2020-08-01 13:35:23 +0200

    :sparkles: Social sharing !

--- assets/js/susty.js
+++ assets/js/susty.js
@@ -10,28 +10,42 @@
 	document.head.appendChild( link );
 })( document );
 
-var windowLinks = document.querySelectorAll("[data-window]");
-
+/**
+ * Open in a new Window.
+ *
+ * @type {NodeListOf<Element>}
+ */
+var windowLinks = document.querySelectorAll( "[data-window]" );
 
 for (var link of windowLinks) {
-	var URI = link.dataset.window;
-
 	link.onclick = function() {
-		popupwindow(URI, "Partager", 500, 500);
+		popupwindow( this.dataset.window, "Partager", 500, 500 );
 	};
 }
 
+/**
+ *
+ * @param url
+ * @param title
+ * @param w
+ * @param h
+ * @returns {Window}
+ */
 function popupwindow(url, title, w, h) {
-	var left = (screen.width/2)-(w/2);
-	var top = (screen.height/2)-(h/2);
-	return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+	var left = (screen.width / 2) - (w / 2);
+	var top  = (screen.height / 2) - (h / 2);
+	return window.open( url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left );
 }
 
-var windowTabs = document.querySelectorAll("[data-tab]");
-for (var link of windowLinks) {
-	var URI = link.dataset.tab;
+/**
+ * Open in a new tab.
+ *
+ * @type {NodeListOf<Element>}
+ */
+var windowTabs = document.querySelectorAll( "[data-tab]" );
 
-	link.onclick = function() {
-		windo.open(URI);
-	};
+for (var tab of windowTabs) {
+	tab.onclick = function () {
+		window.open(this.dataset.tab);
+	}
 }
