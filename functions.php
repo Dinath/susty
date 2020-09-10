2f4e9fab5d9772dce42c99df51a49fede276832d
Alex <alexandre.soyer@codein.fr>
2020-09-10 14:40:10 +0200

    :zap: Disable jQuery in frontend

--- functions.php
+++ functions.php
@@ -194,3 +194,12 @@ function the_content_time_to_read() {
 	</small>
 	<?php
 }
+
+/** * Completely Remove jQuery From WordPress */
+function my_init() {
+	if (!is_admin()) {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', false);
+	}
+}
+add_action('init', 'my_init');
